767981a260def5e088f2bf37ffd751b6f0468ddf
MarcusVini196 <marcusviniciussolleh@gmail.com>
2024-07-22 12:00:00 -0300

    Update configFirebase.js

--- assets/js/configFirebase.js
+++ assets/js/configFirebase.js
@@ -16,13 +16,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxjbzrp6Ui9xfx31u_WspHzf6FefBEX94",
-  authDomain: "clinica-saude-em-dia-d75f7.firebaseapp.com",
-  projectId: "clinica-saude-em-dia-d75f7",
-  storageBucket: "clinica-saude-em-dia-d75f7.appspot.com",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
   messagingSenderId: "410672853545",
-  appId: "1:410672853545:web:babe43f3002e62681a2238",
-  measurementId: "G-0NTH7VEY2G"
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
